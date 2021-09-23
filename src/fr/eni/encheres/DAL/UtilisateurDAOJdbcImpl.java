a17da16bbc4b6edce81148c350ff8bc99dba3084
FlorianCountry <florian.pays@gmail.com>
2021-09-23 11:24:44 +0200

    modifs + méthodes update

--- src/fr/eni/encheres/DAL/UtilisateurDAOJdbcImpl.java
+++ src/fr/eni/encheres/DAL/UtilisateurDAOJdbcImpl.java
@@ -18,18 +18,19 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 	private static final String SELECT_BY_EMAIL = "SELECT * FROM UTILISATEURS WHERE email = ?"; 
 	private static final String SELECT_BY_ID = "SELECT * FROM UTILISATEURS WHERE no_utilisateur = ?";
 	private static final String SELECT_ALL = "SELECT * FROM UTILISATEURS";
-	private static final String INSERT_UTILISATEUR = "INSERT INTO (pseudo, nom, prenom, email, telephone, rue, code_postal, "
+	private static final String INSERT_UTILISATEUR = "INSERT INTO UTILISATEURS (pseudo, nom, prenom, email, telephone, rue, code_postal, "
 			+ "ville, mot_de_passe, credit, administrateur) VALUES(?,?,?,?,?,?,?,?,?,?,?); ";
 	private static final String UPDATE_UTILISATEUR = "UPDATE UTILISATEURS SET pseudo=?, nom=?, prenom=?"
 			+ "email=?, telephone=?, ville=?, mot_de_passe=? WHERE no_utilisateur = ?";
-	private static final String UPDATE_CREDIT_UTILISATEUR = "UPDATE UTILISATEURS SET credit=? WHERE no_utilisateur = ?";
+	private static final String UPDATE_CREDIT_UTILISATEUR = "UPDATE UTILISATEURS SET credit= ? WHERE no_utilisateur = ?";
+	private static final String UPDATE_ADMIN_UTILISATEUR = "UPDATE UTILISATEURS SET administrateur= ? WHERE no_utilisateur = ?";
 	private static final String DELETE_UTILISATEUR = "DELETE FROM UTILISATEURS WHERE no_utilisateur=?";
 	
 	
 	private static Utilisateur getUtilisateur(ResultSet rs) throws SQLException 
 	{
 		Utilisateur utilisateur = null;
-		utilisateur = new Utilisateur(rs.getString("no_utilisateur"), rs.getString("pseudo") , rs.getString("nom")
+		utilisateur = new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo") , rs.getString("nom"), rs.getString("prenom")
 				,rs.getString("email"), rs.getString("telephone"), rs.getString("rue"), rs.getString("code_postal")
 				, rs.getString("ville"), rs.getString("mot_de_passe"), rs.getInt("credit"), rs.getBoolean("administrateur"));
 		
@@ -250,16 +251,32 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 		try {
 			cnx = ConnectionProvider.getConnection();
 			pstmt = cnx.prepareStatement(UPDATE_CREDIT_UTILISATEUR);
-			pstmt.setString(1, utilisateur.getPseudo());
-			pstmt.setString(2, utilisateur.getNom());
-			pstmt.setString(3, utilisateur.getPrenom());
-			pstmt.setString(4, utilisateur.getEmail());
-			pstmt.setString(5, utilisateur.getTelephone());
-			pstmt.setString(6, utilisateur.getRue());
-			pstmt.setString(7, utilisateur.getCodePostal());
-			pstmt.setString(8, utilisateur.getVille());
-			pstmt.setString(9, utilisateur.getMotDePasse());
-			
+			pstmt.setInt(1, utilisateur.getCredit());
+			pstmt.executeUpdate();
+		}
+		catch (Exception e){
+			BusinessException businessException = new BusinessException();
+			businessException.addError(CodesResultatDAL.UPDATE_UTILISATEUR_FAIL);				
+			e.printStackTrace();
+			throw businessException;
+		}
+		return utilisateur;
+	}
+	
+	@Override
+	public Utilisateur updateAdmin(Utilisateur utilisateur) throws BusinessException{
+		if (utilisateur == null) 
+		{
+			BusinessException businessException = new BusinessException();
+			businessException.addError(CodesResultatDAL.UTILISATEUR_NULL);
+			throw businessException;
+		}
+		PreparedStatement pstmt = null;
+		Connection cnx = null;
+		try {
+			cnx = ConnectionProvider.getConnection();
+			pstmt = cnx.prepareStatement(UPDATE_ADMIN_UTILISATEUR);
+			pstmt.setBoolean(1, utilisateur.isAdministrateur());
 			pstmt.executeUpdate();
 		}
 		catch (Exception e){
@@ -293,7 +310,7 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 		}
 	}
 	
-	//UPDATE CREDIT + UPDATE ADMIN A FAIRE
+	//UPDATE ADMIN A FAIRE
 	
 	
 }
