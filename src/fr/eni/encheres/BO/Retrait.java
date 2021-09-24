110068b2e43bcc102e24e865dc1f6bfa91ce9097
Lea Terrien <info@lea-terrien.com>
2021-09-24 15:57:31 +0200

    création entités BO liées aux enchères

---
+++ src/fr/eni/encheres/BO/Retrait.java
@@ -0,0 +1,77 @@
+package fr.eni.encheres.BO;
+
+public class Retrait {
+
+	private String rue;
+	private String codePostal;
+	private String ville;
+
+	/**
+	 * @param rue
+	 * @param codePostal
+	 * @param ville
+	 */
+	public Retrait(String rue, String codePostal, String ville) {
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
+	/**
+	 * @return the rue
+	 */
+	public String getRue() {
+		return rue;
+	}
+
+	/**
+	 * @param rue
+	 *            the rue to set
+	 */
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	/**
+	 * @return the codePostal
+	 */
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	/**
+	 * @param codePostal
+	 *            the codePostal to set
+	 */
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+
+	/**
+	 * @param ville
+	 *            the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Retrait [" + (rue != null ? "rue=" + rue + ", " : "")
+				+ (codePostal != null ? "codePostal=" + codePostal + ", " : "")
+				+ (ville != null ? "ville=" + ville : "") + "]";
+	}
+
+}
