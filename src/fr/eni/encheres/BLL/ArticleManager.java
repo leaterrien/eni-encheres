95630bf33dbe60b40c5c6346816ce3dc39f98696
Lea Terrien <info@lea-terrien.com>
2021-09-28 09:43:21 +0200

    WIP ArticleManager

--- src/fr/eni/encheres/BLL/ArticleManager.java
+++ src/fr/eni/encheres/BLL/ArticleManager.java
@@ -1,6 +1,14 @@
 package fr.eni.encheres.BLL;
 
+import fr.eni.encheres.DAL.ArticleDAO;
+
 public class ArticleManager {
-	
-	
+
+	private static ArticleManager instance;
+	private ArticleDAO articleDAO;
+
+	private ArticleManager() {
+
+	}
+
 }
